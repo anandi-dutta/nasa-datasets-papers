0f966fef06db19a91164206e7164c2c0254c1b31
anandi-dutta <anandixd@gmail.com>
2026-04-08 14:47:50 -0500

    Add NASA data for Mars and Moon datasets

---
+++ data.js
@@ -0,0 +1,39 @@
+const nasaData = [
+  {
+    "planet": "Mars",
+    "dataset": "High Resolution Imaging Science Experiment (HiRISE)",
+    "link": "https://www.uahirise.org/ESP_089986_1980",
+    "papers": [
+      {
+        "title": "Mars Reconnaissance Orbiter's High Resolution Imaging Science Experiment (HiRise)",
+        "url": "https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2005JE002605"
+      },
+      {
+        "title": "A classification of Martian gullies from HiRISE imagery",
+        "url": "https://www.sciencedirect.com/science/article/pii/S003206331530091X"
+      }
+    ]
+  },
+  {
+    "planet": "Mars",
+    "dataset": "The Global Context Camera (CTX) Mosaic of Mars",
+    "link": "https://murray-lab.caltech.edu/CTX/",
+    "papers": [
+      {
+        "title": "The Global Context Camera (CTX) Mosaic of Mars: A Product of Information-Preserving Image Data Processing",
+        "url": "https://agupubs.onlinelibrary.wiley.com/doi/10.1029/2024EA003555"
+      }
+    ]
+  },
+  {
+    "planet": "Moon",
+    "dataset": "The Lunar Orbiter Laser Altimeter (LOLA)",
+    "link": "https://imbrium.mit.edu/",
+    "papers": [
+      {
+        "title": "The steepest slopes on the Moon from Lunar Orbiter Laser Altimeter (LOLA) data",
+        "url": "https://www.sciencedirect.com/science/article/pii/S0019103516001147"
+      }
+    ]
+  }
+];
